167a5ab852af441a3db5952d72e03d02a6909ad9
SKYoooon <skyoon7899@gmail.com>
2024-02-19 15:16:05 +0900

    -

--- HR/SQL_Test/challenge.sql
+++ HR/SQL_Test/challenge.sql
@@ -19,3 +19,32 @@ ORDER BY
     DAY(o.invoicedate) ASC,
     o.customerid ASC;
    
+
+-- 구매금액 동일 추가
+SELECT 
+    YEAR(o.invoicedate) AS Year,
+    MONTH(o.invoicedate) AS Month,
+    DAY(o.invoicedate) AS Day,
+    o.customerid AS CustomerID,
+    MIN(od.total_payment) AS TotalPayment
+FROM 
+    orders o
+JOIN 
+    (SELECT 
+         invoiceno,
+         SUM(quantity * unitprice) AS total_payment
+     FROM 
+         order_details
+     GROUP BY 
+         invoiceno) od ON o.invoiceno = od.invoiceno
+GROUP BY 
+    YEAR(o.invoicedate),
+    MONTH(o.invoicedate),
+    DAY(o.invoicedate),
+    o.customerid
+ORDER BY 
+    Year ASC,
+    Month ASC,
+    Day ASC,
+    CustomerID ASC;
+
